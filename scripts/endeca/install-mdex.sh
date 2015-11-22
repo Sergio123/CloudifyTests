e266100a44ef1ec27dc0739d9cd91b32e5ea8bf1
Sergio <tsergey.t@gmail.com>
2015-11-22 18:03:30 +0200

    add ftp configuration

--- scripts/endeca/install-mdex.sh
+++ scripts/endeca/install-mdex.sh
@@ -2,14 +2,19 @@
 set -e
 function download() {
 	url=$1
-	name=$2
-	ctx logger info "download ${url} ${name}"
+	user=$2
+	password=$3
+	ctx logger info "download ${url} ${user} ${password}"
 }
 
 function untar() {
 	ctx logger info "untar"
 }
 
-TEMP_DIR='/tmp'
 
+TEMP_DIR='/tmp'
+FTP_URL= $(ctx node properties ftp url)
+FTP_USER= $(ctx node properties ftp user)
+FTP_PASSWORD= $(ctx node properties ftp password)
+download ${FTP_URL} ${FTP_USER} ${FTP_PASSWORD}
 ctx logger info "Successfully installed mdex"
